f83088488d0da30a11b75ba3be5c8ead962d7a94
GugaZC <gustavo.z.canal@gmail.com>
2020-11-14 17:42:51 -0300

    Co-authored-by: Julio Carraro <julio.carraro@outlook.com>

--- client/client.js
+++ client/client.js
@@ -1,83 +1,168 @@
 let socket;
 
-function connectToServer(url) {
+const connectToServer = (url) => {
     socket = new WebSocket(url);
 
     socket.onopen = () => {
         const data = {
-            action: "getPlayers"
-        }
+            action: "getPlayers",
+        };
         socket.send(JSON.stringify(data));
-    }
+    };
 
-    socket.onmessage =  event => {
-        const dataRecived = JSON.parse(event.data)
+    socket.onmessage = (event) => {
+        const dataRecived = JSON.parse(event.data);
         switch (dataRecived.action) {
             case "updateUsersStatus":
-                updateUsers(dataRecived.users)
+                updateUsers(dataRecived.users);
                 break;
-            
+
             case "selfChallenge":
-                alert("Você não pode se desafiar")
+                alert("Você não pode se desafiar");
                 break;
 
             case "playerNotAvailable":
-                alert("Esse player não está disponível")
+                alert("Esse player não está disponível");
                 break;
 
             case "acceptChallenge":
-                alert(`Você foi desafiado pelo ${dataRecived.player}`)
+                handleChallenge(dataRecived.player1, dataRecived.player2);
                 break;
 
+            case "enterGame":
+                enterGame(dataRecived.game.id);
+                break;
+
+            case "challengeRefused":
+                alert("Seu desafio foi recusado");
+                break;
+            case "updateGame":
+                updateGame(dataRecived.cellId);
+                break;
             default:
                 break;
         }
-        // const message = json.data
-
-        // const cell = document.getElementById(message.id);
-
-        // cell.innerHTML = `${message.symbol}`
-    }
-}
+    };
+};
 
 const updateUsers = (data) => {
-    const list = document.getElementById("players-online")
-    list.innerHTML = ''
-    data.available.forEach( element => {
-        list.insertAdjacentHTML("beforeend",`<li> <button type="button" id="${element.id}"  onclick="challenge(this.id)" class="player status-${element.status}">${element.name}</button> </li>`)
-    })
-}
+    const list = document.getElementById("players-online");
+    list.innerHTML = "";
+    data.available.forEach((element) => {
+        list.insertAdjacentHTML(
+            "beforeend",
+            `<li> <button type="button" id="${element.id}"  onclick="challenge(this.id)" class="player status-${element.status}">${element.name}</button> </li>`
+        );
+    });
+};
 
 const challenge = (id) => {
     const data = {
         action: "challengePlayer",
-        challengedPlayerId:id, 
-    }
-    socket.send(JSON.stringify(data))
-}
+        challengedPlayerId: id,
+    };
+    socket.send(JSON.stringify(data));
+};
 
-const updateCell = id => {
-    const cell = document.getElementById(id);
+const updateCell = (cellId, boardId) => {
+    // const cell = document.getElementById(id);
 
     const data = {
         symbol: "x",
         action: "updateCell",
-        id
+        cellId,
+        boardId,
+    };
+
+    socket.send(JSON.stringify(data));
+};
+
+const updateGame = (cellId) => {
+    const cells = document.getElementsByClassName("cell");
+    const cell = Array.prototype.filter.call(
+        cells,
+        (cell) => cell.id === cellId
+    )[0];
+
+    cell.innerHTML = "X";
+};
+
+const handleChallenge = (player1, player2) => {
+    const answer = confirm(`Você foi desafiado pelo ${player1.name}`);
+
+    if (answer == true) {
+        const data = {
+            action: "createGame",
+            player1: `${player1.id}`,
+            player2: `${player2.id}`,
+        };
+
+        socket.send(JSON.stringify(data));
+    } else {
+        const data = {
+            action: "refuseChallenge",
+            player: player1.id,
+        };
+        socket.send(JSON.stringify(data));
     }
+};
 
-    socket.send(JSON.stringify(data))
-}
+const enterGame = (id) => {
+    const board = `<table id="${id}" class="center">
+        <tr>
+            <td>
+                <button type="button" class="cell" id="cell00"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell01"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell02"></button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button type="button" class="cell" id="cell10"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell11"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell12"></button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button type="button" class="cell" id="cell20"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell21"></button>
+            </td>
+            <td>
+                <button type="button" class="cell" id="cell22"></button>
+            </td>
+        </tr>
+    </table>`;
 
-document.addEventListener("DOMContentLoaded", event => {
+    const boardContainer = document.getElementById("game-screen");
 
-    connectToServer('ws://localhost:8080');
+    const lobby = document.getElementById("lobby-screen");
+
+    lobby.className = "center hidden";
+
+    boardContainer.className = "center";
+
+    boardContainer.innerHTML = board;
 
     const cells = document.getElementsByClassName("cell");
 
-    Array.prototype.forEach.call(cells, element => {
-        element.addEventListener('click', event => {
-            updateCell(element.id)
-        })
+    Array.prototype.forEach.call(cells, (element) => {
+        element.addEventListener("click", (event) => {
+            updateCell(element.id, id);
+        });
     });
+};
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    connectToServer("ws://localhost:8080");
 });
--- server.js
+++ server.js
@@ -1,39 +1,49 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 }, function () {
-    console.log('server on');
+    console.log("server on");
 });
 
 let socketsConnected = [];
+let games = [];
+const TIMEOUT = 10000;
 
-wss.on('connection', ws => {
+wss.on("connection", (ws) => {
     const Player = {
-        id:`${Math.round(Math.random()*10000)}`,
-        name:`Player${socketsConnected.length + 1}`,
+        id: `${Math.round(Math.random() * 10000)}`,
+        name: `Player${socketsConnected.length + 1}`,
         status: "available",
-    }
+        // connectedAt: Date.now()
+    };
 
-    ws.Player = Player
+    ws.Player = Player;
     socketsConnected.push(ws);
 
-    ws.on('message', data => {
-
+    ws.on("message", (data) => {
         // data = {
         //     action: string,  ação que deve ser feita
         //     cellId?: string  id da celula que foi clicada
-        //     symbol?: string  símbolo que deve ser inserido 
+        //     symbol?: string  símbolo que deve ser inserido
         //     player1Id?:
         //     player2Id?:
         //     challengedPlayerId?: se for mensagem pra desafio terá o id do player que é desafiado
         // }
 
-        const {action, challengedPlayerId} = JSON.parse(data);
-        
+        const {
+            action,
+            challengedPlayerId,
+            player1,
+            player2,
+            player,
+            cellId,
+            boardId,
+        } = JSON.parse(data);
+
         const senderId = ws.Player.id;
 
         switch (action) {
             case "updateCell":
-                console.log("updatecell");
+                updateCell(cellId, boardId);
                 break;
 
             case "getPlayers":
@@ -45,63 +55,154 @@ wss.on('connection', ws => {
                 break;
 
             case "challengePlayer":
-                if(challengedPlayerId === senderId){
+                if (challengedPlayerId === senderId) {
                     const data = {
-                        action:"selfChallenge"
-                    }
-                    ws.send(JSON.stringify(data))
+                        action: "selfChallenge",
+                    };
+                    ws.send(JSON.stringify(data));
                     break;
                 }
                 challengePlayer(challengedPlayerId, senderId);
                 break;
+            case "createGame":
+                createGame(player1, player2);
+                break;
+
+            case "refuseChallenge":
+                const data = {
+                    action: "challengeRefused",
+                };
+                const Player = socketsConnected.filter(
+                    (socket) => socket.Player.id === player
+                )[0];
+
+                Player.send(JSON.stringify(data));
+                break;
             default:
                 break;
         }
-
     });
 
-    ws.on('close', function connectionLost(element) {
-
+    ws.on("close", (element) => {
         const index = socketsConnected.indexOf(ws);
         socketsConnected.splice(index, 1);
-
-    })
+    });
 });
 
 const sendUsersConnected = () => {
     let data = {
         action: "updateUsersStatus",
-        users:{
-            available:[],
-        }
+        users: {
+            available: [],
+        },
     };
 
-    socketsConnected.forEach((socket, index) => {
-        data.users.available.push(socket.Player)
+    socketsConnected.forEach((socket) => {
+        data.users.available.push(socket.Player);
     });
-    socketsConnected.forEach((socket)=>{
-        socket.send(JSON.stringify(data))
-    })
-}
+    socketsConnected.forEach((socket) => {
+        socket.send(JSON.stringify(data));
+    });
+};
 
 const challengePlayer = (challengedId, challengerId) => {
+    const challenged = socketsConnected.filter(
+        (socket) => socket.Player.id === challengedId
+    )[0];
 
-    const challenged = socketsConnected.filter(socket => socket.Player.id === challengedId)[0]
+    const challenger = socketsConnected.filter(
+        (socket) => socket.Player.id === challengerId
+    )[0];
 
-    const challenger = socketsConnected.filter(socket => socket.Player.id === challengerId)[0]
-
-    if(challenged.Player.status != "available"){
-        const data ={
-            action: "playerNotAvailable"
-        }
-        challenger.send(JSON.stringify(data))
-        return
+    if (challenged.Player.status != "available") {
+        const data = {
+            action: "playerNotAvailable",
+        };
+        challenger.send(JSON.stringify(data));
+        return;
     }
 
     const data = {
-        action: "acceptChallenge", 
-        player: challenger.Player.name
-    }
+        action: "acceptChallenge",
+        player1: {
+            name: challenger.Player.name,
+            id: challenger.Player.id,
+        },
+        player2: {
+            id: challenged.Player.id,
+        },
+    };
 
-    challenged.send(JSON.stringify(data))
-}
+    challenged.send(JSON.stringify(data));
+};
+
+const createGame = (player1, player2) => {
+    const newGame = {
+        id: `${games.length + 1}`,
+        player1Id: player1,
+        player2Id: player2,
+    };
+
+    games.push(newGame);
+
+    const Player1 = socketsConnected.filter(
+        (socket) => socket.Player.id === player1
+    )[0];
+
+    const Player2 = socketsConnected.filter(
+        (socket) => socket.Player.id === player2
+    )[0];
+
+    const data = {
+        action: "enterGame",
+        game: {
+            id: newGame.id,
+        },
+    };
+    Player1.Player.status = "inGame";
+    Player2.Player.status = "inGame";
+
+    sendUsersConnected();
+
+    Player1.send(JSON.stringify(data));
+    Player2.send(JSON.stringify(data));
+};
+
+const updateCell = (cellId, boardId) => {
+    const board = games.filter((game) => game.id === boardId)[0];
+
+    const Player1 = socketsConnected.filter(
+        (socket) => socket.Player.id === board.player1Id
+    )[0];
+
+    const Player2 = socketsConnected.filter(
+        (socket) => socket.Player.id === board.player2Id
+    )[0];
+
+    const data = {
+        action: "updateGame",
+        cellId,
+    };
+
+    Player1.send(JSON.stringify(data));
+    Player2.send(JSON.stringify(data));
+};
+
+const periodic = () => {
+    let actualTime = Date.now();
+
+    let x = 0;
+
+    while (x < socketsConnected.lenght) {
+        if (
+            socketsConnected[x].validate == false &&
+            actualTime - socketsConnected[x].timestamp > TIMEOUT
+        ) {
+            console.log("User removido dos ativos");
+
+            let MSG = { tipo: "ERRO", valor: "timeout" };
+            socketsConnected[x].close();
+            socketsConnected.splice(x, 1);
+        } else x++;
+    }
+};
